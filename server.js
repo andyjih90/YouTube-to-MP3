c3b9f0d06b1b4204f6f88d537147981473d3eab4
andyjih90 <andyjih90@gmail.com>
2024-06-09 18:14:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
 const NodeID3 = require("node-id3");
 const axios = require("axios");
+const ytpl = require("ytpl");
 
 const app = express();
 const port = 3000;
@@ -21,6 +22,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/playlist.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "playlist.html"));
 });
 
 function sanitizeFilename(filename) {
@@ -125,6 +130,55 @@ app.post("/convert", async (req, res) => {
     res.status(500).json({
       error: "Conversion error. Please enter a valid link and try again.",
     });
+  }
+});
+
+app.post("/convertPlaylist", async (req, res) => {
+  const playlistUrl = req.body["playlist-url"];
+
+  console.log("Received request to convert playlist:", playlistUrl);
+
+  try {
+    const playlist = await ytpl(playlistUrl, { pages: 1 });
+    const downloadUrls = [];
+
+    for (const item of playlist.items) {
+      const info = await ytdl.getInfo(item.shortUrl);
+      let title = info.videoDetails.title;
+      const artist = info.videoDetails.author.name;
+
+      title = title.replace(/\(.*?\)/g, "").trim();
+
+      const safeTitle = sanitizeFilename(title);
+      const outputFilePath = path.join(
+        __dirname,
+        "downloads",
+        `${safeTitle}.mp3`
+      );
+
+      const stream = ytdl(item.shortUrl, { quality: "highestaudio" });
+      await new Promise((resolve, reject) => {
+        ffmpeg(stream)
+          .audioBitrate(128)
+          .save(outputFilePath)
+          .on("end", () => resolve())
+          .on("error", (err) => reject(err));
+      });
+
+      downloadUrls.push(`/download?file=${encodeURIComponent(outputFilePath)}`);
+    }
+
+    res.json({
+      message: "Playlist conversion completed!",
+      downloadUrl: `/playlistResults.html?urls=${encodeURIComponent(
+        JSON.stringify(downloadUrls)
+      )}`,
+    });
+  } catch (error) {
+    console.error("Playlist conversion error:", error);
+    res
+      .status(500)
+      .json({ error: "Playlist conversion error. Please try again." });
   }
 });
 
